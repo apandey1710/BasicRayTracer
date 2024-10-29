2a30542ac65fd870c45243848e49982684fda25e
apandey1710 <lastsonofkrypton754@gmail.com>
2024-10-28 18:07:14 -0700

    Add Ray class and include it in ray.cpp and ray.h

---
+++ imaging/ray.h
@@ -0,0 +1,26 @@
+
+#ifndef _RAY_H
+#define _RAY_H
+
+#include <vec.h>
+
+class Ray {
+public:
+    Ray() {}
+    Ray(const Point3& origin, const Vec3& direction)
+        : orig(origin), dir(direction)
+    {}
+
+    Point3 origin() const { return orig; }
+    Vec3 direction() const { return dir; }
+
+    Point3 at(double t) const {
+        return orig + t * dir;
+    }
+
+private:
+        Point3 orig;
+        Vec3 dir;
+}
+
+#endif
